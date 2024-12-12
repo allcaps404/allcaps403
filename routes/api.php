a4afdae3db00068128f0645897d37a54d90fe94d
allcaps404 <allcaps403@gmail.com>
2024-12-12 18:21:18 +0800

    modify in environmentdatacontroller

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::prefix('logs')->group(function () {
-    Route::post('/log/store', [EnvironmentDataController::class, 'store'])->name('environment.store');
+    Route::get('/log/store', [EnvironmentDataController::class, 'store'])->name('environment.store');
     // Route::get('/log/store/{temperature}/{humidity}/{soil_moisture_1}/{soil_moisture_2}/{soil_moisture_3}/{avg_soil_moisture}', [EnvironmentDataController::class, 'store'])->name('environment.store');
     Route::get('/latest-environment-data', [EnvironmentDataController::class, 'getLatestData']);
 
