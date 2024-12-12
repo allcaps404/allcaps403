ba830772c74a90c8c1a7401988ce290f4d333d10
allcaps404 <allcaps403@gmail.com>
2024-12-12 17:18:54 +0800

    slight

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -8,10 +8,14 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Admin</title>
   <!-- plugins:css -->
+  <link rel="stylesheet" href="{{asset('mytemplate/vendors/feather/feather.css')}}">
   <link rel="stylesheet" href="{{asset('mytemplate/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('mytemplate/vendors/css/vendor.bundle.base.css')}}">
   <!-- endinject -->
   <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{asset('mytemplate/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
   <link rel="stylesheet" href="{{asset('mytemplate/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('mytemplate/js/select.dataTables.min.css')}}">
   <!-- End plugin css for this page -->
   <!-- inject:css -->
   <link rel="stylesheet" href="{{asset('mytemplate/css/vertical-layout-light/style.css')}}">
@@ -114,10 +118,19 @@
   </div>
   <!-- container-scroller -->
 
+  <!-- plugins:js -->
+  <script src="{{asset('mytemplate/vendors/js/vendor.bundle.base.js')}}"></script>
+  <!-- endinject -->
   <!-- Plugin js for this page -->
   <script src="{{asset('mytemplate/vendors/chart.js/Chart.min.js')}}"></script>
   <!-- End plugin js for this page -->
-
+  <!-- inject:js -->
+  <script src="{{asset('mytemplate/js/off-canvas.js')}}"></script>
+  <script src="{{asset('mytemplate/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('mytemplate/js/template.js')}}"></script>
+  <script src="{{asset('mytemplate/js/settings.js')}}"></script>
+  <script src="{{asset('mytemplate/js/todolist.js')}}"></script>
+  <!-- endinject -->
   <!-- Custom js for this page-->
   <script src="{{asset('mytemplate/js/dashboard.js')}}"></script>
   <script src="{{asset('mytemplate/js/Chart.roundedBarCharts.js')}}"></script>
